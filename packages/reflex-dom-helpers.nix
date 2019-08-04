ccc51bac3711d475af1d0f6d1decf8e52db396e9
Jappie Klooster <jappieklooster@hotmail.com>
2019-08-04 18:21:30 +0200

    Update reflex dom helpers

--- packages/reflex-dom-helpers.nix
+++ packages/reflex-dom-helpers.nix
@@ -1,20 +1,20 @@
-{ mkDerivation, base, fetchgit, reflex, reflex-dom, stdenv
+{ mkDerivation, base, fetchgit, reflex, reflex-dom-core, stdenv
 , template-haskell, text
 }:
 mkDerivation {
   pname = "reflex-dom-helpers";
-  version = "0.2.0.0";
+  version = "0.2.0.1";
   src = fetchgit {
-    url = "https://github.com/jappeace/reflex-dom-helpers";
-    sha256 = "0w3i424pp8in1271q7wp44bkxfcq7f8m3ivj5rpjk9i58zmjb2f4";
-    rev = "112901c67a3c58c065469cfd45dd2d7ab8509ae6";
+    url = "https://github.com/layer-3-communications/reflex-dom-helpers";
+    sha256 = "12k5jr1rif2yi1xa4a43gx1rchj1ywmh4m1wdj00mjslisjdndgm";
+    rev = "e247c3ee5f9d957f994aeb57b9fe4d9f0a35beea";
     fetchSubmodules = true;
   };
   libraryHaskellDepends = [
-    base reflex reflex-dom template-haskell text
+    base reflex reflex-dom-core template-haskell text
   ];
   testHaskellDepends = [ base ];
   homepage = "https://github.com/layer-3-communications/reflex-dom-helpers";
-  description = "Element tag helpers for working with reflex-dom";
+  description = "Html tag helpers for reflex-dom";
   license = stdenv.lib.licenses.bsd3;
 }
